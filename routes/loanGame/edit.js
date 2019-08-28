aa54fbdf5e63e8c2f3455ecbb9bfcb49894805ec
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-08-28 03:59:35 +0200

    edit

--- routes/loanGame/edit.js
+++ routes/loanGame/edit.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const { LoanGame } = require("../../models");
-router.get("/:loanGameId", function(req, res, next) {
-  const now =new Date();
+router.get("/:loanGameId/:hireUserId", function(req, res, next) {
+  const now = new Date();
   LoanGame.update(
     {
       endLoan: now,
-      deletedAt: null
+      deletedAt: null,
+      hireUserId: req.params.hireUserId
     },
     { where: { id: req.params.loanGameId } }
   )
