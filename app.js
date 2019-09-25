634d9e53cc70a7aaf8fee5039f6a51ac54b33b78
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-09-25 21:03:11 +0200

    add endpoint

--- app.js
+++ app.js
@@ -1,117 +1,189 @@
-var createError = require('http-errors');
-var express = require('express');
-var cors = require('cors')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const index = require('./routes/home');
-const { Auth, BoardGame, BoardGameMechanic, BoardGameType, LoanGame, Mechanic, Publisher, Type, User } = require('./routes');
+var createError = require("http-errors");
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const index = require("./routes/home");
+const {
+  Auth,
+  BoardGame,
+  BoardGameMechanic,
+  BoardGameType,
+  LoanGame,
+  Mechanic,
+  Publisher,
+  Type,
+  User,
+  Event,
+  Feedback,
+  Lecture,
+  Reservation,
+  Table
+} = require("./routes");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(cors())
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
+app.use("/", index);
 
 //auth
-app.use('/login', Auth.login);
-app.use('/register', Auth.register);
-app.use('/changePassword',Auth.changePassword);
-app.use('/changePrivilege',Auth.changePrivilege);
-app.use('/logout',Auth.logout);
+app.use("/login", Auth.login);
+app.use("/register", Auth.register);
+app.use("/changePassword", Auth.changePassword);
+app.use("/changePrivilege", Auth.changePrivilege);
+app.use("/logout", Auth.logout);
 
 //user
-app.use('/users', User.all);
-app.use('/user/edit', User.edit);
-app.use('/user/remove', User.remove);
-app.use('/user/offset', User.offset);
-app.use('/user/search', User.search);
-app.use('/user', User.get);
+app.use("/users", User.all);
+app.use("/user/edit", User.edit);
+app.use("/user/remove", User.remove);
+app.use("/user/offset", User.offset);
+app.use("/user/search", User.search);
+app.use("/user", User.get);
 
 //boardGame
-app.use('/boardGames', BoardGame.all);
-app.use('/boardGame', BoardGame.get);
-app.use('/boardGame/remove', BoardGame.remove);
-app.use('/boardGame/add', BoardGame.add);
-app.use('/boardGame/edit', BoardGame.edit);
-app.use('/boardGame/offset', BoardGame.offset);
-app.use('/boardGame/available', BoardGame.available);
-app.use('/boardGame/search', BoardGame.search);
-app.use('/boardGame/search/uuid', BoardGame.searchByUUID);
-app.use('/boardGame/file', BoardGame.uploadFile);
+app.use("/boardGames", BoardGame.all);
+app.use("/boardGame", BoardGame.get);
+app.use("/boardGame/remove", BoardGame.remove);
+app.use("/boardGame/add", BoardGame.add);
+app.use("/boardGame/edit", BoardGame.edit);
+app.use("/boardGame/offset", BoardGame.offset);
+app.use("/boardGame/available", BoardGame.available);
+app.use("/boardGame/search", BoardGame.search);
+app.use("/boardGame/search/uuid", BoardGame.searchByUUID);
+app.use("/boardGame/file", BoardGame.uploadFile);
 
 //boardGameMechanic
-app.use('/boardGameMechanics', BoardGameMechanic.all);
-app.use('/boardGameMechanic', BoardGameMechanic.get);
-app.use('/boardGameMechanic/remove', BoardGame.remove);
-app.use('/boardGameMechanic/add', BoardGame.add);
-app.use('/boardGameMechanic/edit', BoardGame.edit);
-app.use('/boardGameMechanic/offset', BoardGame.offset);
+app.use("/boardGameMechanics", BoardGameMechanic.all);
+app.use("/boardGameMechanic", BoardGameMechanic.get);
+app.use("/boardGameMechanic/remove", BoardGame.remove);
+app.use("/boardGameMechanic/add", BoardGame.add);
+app.use("/boardGameMechanic/edit", BoardGame.edit);
+app.use("/boardGameMechanic/offset", BoardGame.offset);
 
 //boardGameType
-app.use('/boardGameTypes', BoardGameType.all);
-app.use('/boardGameType', BoardGameType.get);
-app.use('/boardGameType/remove', BoardGameType.remove);
-app.use('/boardGameType/add', BoardGameType.add);
-app.use('/boardGameType/edit', BoardGameType.edit);
-app.use('/boardGameType/offset', BoardGameType.offset);
+app.use("/boardGameTypes", BoardGameType.all);
+app.use("/boardGameType", BoardGameType.get);
+app.use("/boardGameType/remove", BoardGameType.remove);
+app.use("/boardGameType/add", BoardGameType.add);
+app.use("/boardGameType/edit", BoardGameType.edit);
+app.use("/boardGameType/offset", BoardGameType.offset);
 
 //loanGame
-app.use('/loanGames', LoanGame.all);
-app.use('/loanGame/edit', LoanGame.edit);
-app.use('/loanGame/add', LoanGame.add);
-app.use('/loanGame/remove', LoanGame.remove);
-app.use('/loanGame/offset', LoanGame.offset);
-app.use('/loanGame', LoanGame.get);
-app.use('/loanGame/user', LoanGame.byUser);
+app.use("/loanGames", LoanGame.all);
+app.use("/loanGame/edit", LoanGame.edit);
+app.use("/loanGame/add", LoanGame.add);
+app.use("/loanGame/remove", LoanGame.remove);
+app.use("/loanGame/offset", LoanGame.offset);
+app.use("/loanGame", LoanGame.get);
+app.use("/loanGame/user", LoanGame.byUser);
 
 //mechanic
-app.use('/mechanics', Mechanic.all);
-app.use('/mechanic/edit', Mechanic.edit);
-app.use('/mechanic/add', Mechanic.add);
-app.use('/mechanic/remove', Mechanic.remove);
-app.use('/mechanic/offset', Mechanic.offset);
-app.use('/mechanic', Mechanic.get);
+app.use("/mechanics", Mechanic.all);
+app.use("/mechanic/edit", Mechanic.edit);
+app.use("/mechanic/add", Mechanic.add);
+app.use("/mechanic/remove", Mechanic.remove);
+app.use("/mechanic/offset", Mechanic.offset);
+app.use("/mechanic", Mechanic.get);
 
 //publisher
-app.use('/publishers', Publisher.all);
-app.use('/publisher/edit', Publisher.edit);
-app.use('/publisher/add', Publisher.add);
-app.use('/publisher/remove', Publisher.remove);
-app.use('/publisher/offset', Publisher.offset);
-app.use('/publisher', Publisher.get);
+app.use("/publishers", Publisher.all);
+app.use("/publisher/edit", Publisher.edit);
+app.use("/publisher/add", Publisher.add);
+app.use("/publisher/remove", Publisher.remove);
+app.use("/publisher/offset", Publisher.offset);
+app.use("/publisher", Publisher.get);
 
 //type
-app.use('/types', Type.all);
-app.use('/type/edit', Type.edit);
-app.use('/type/add', Type.add);
-app.use('/type/remove', Type.remove);
-app.use('/type/offset', Type.offset);
-app.use('/type', Type.get);
+app.use("/types", Type.all);
+app.use("/type/edit", Type.edit);
+app.use("/type/add", Type.add);
+app.use("/type/remove", Type.remove);
+app.use("/type/offset", Type.offset);
+app.use("/type", Type.get);
+
+//table
+app.use("/tables", Table.all);
+app.use("/table/edit", Table.edit);
+app.use("/table/add", Table.add);
+app.use("/table/remove", Table.remove);
+app.use("/table/offset", Table.offset);
+app.use("/table", Table.get);
+
+
+//table
+app.use("/tables", Table.all);
+app.use("/table/edit", Table.edit);
+app.use("/table/add", Table.add);
+app.use("/table/remove", Table.remove);
+app.use("/table/offset", Table.offset);
+app.use("/table", Table.get);
+
+//event
+app.use("/events", Table.all);
+app.use("/event/edit", Table.edit);
+app.use("/event/add", Table.add);
+app.use("/event/remove", Table.remove);
+app.use("/event/offset", Table.offset);
+app.use("/event", Table.get);
+
+//event
+app.use("/events", Event.all);
+app.use("/event/edit", Event.edit);
+app.use("/event/add", Event.add);
+app.use("/event/remove", Event.remove);
+app.use("/event/offset", Event.offset);
+app.use("/event", Event.get);
+
+//feedback
+app.use("/feedbacks", Feedback.all);
+app.use("/feedback/edit", Feedback.edit);
+app.use("/feedback/add", Feedback.add);
+app.use("/feedback/remove", Feedback.remove);
+app.use("/feedback/offset", Feedback.offset);
+app.use("/feedback", Feedback.get);
+
+//lecture
+app.use("/lectures", Lecture.all);
+app.use("/lecture/edit", Lecture.edit);
+app.use("/lecture/add", Lecture.add);
+app.use("/lecture/remove", Lecture.remove);
+app.use("/lecture/offset", Lecture.offset);
+app.use("/lecture", Lecture.get);
+
+//reservation
+app.use("/reservations", Reservation.all);
+app.use("/reservation/edit", Reservation.edit);
+app.use("/reservation/add", Reservation.add);
+app.use("/reservation/remove", Reservation.remove);
+app.use("/reservation/offset", Reservation.offset);
+app.use("/reservation", Reservation.get);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,29 @@ const BoardGame = require("./boardGame");
 const BoardGameType = require("./boardGameType");
 const BoardGameMechanic = require("./boardGameMechanic");
 const LoanGame = require("./loanGame");
-const Publisher = require('./publisher');
-const Mechanic = require('./mechanic');
-const Type = require('./type');
-const User = require('./user');
+const Publisher = require("./publisher");
+const Mechanic = require("./mechanic");
+const Type = require("./type");
+const User = require("./user");
+const Table = require("./table");
+const Reservation = require("./reservation");
+const Event = require("./event");
+const Feedback = require("./feedback");
+const Lecture = require("./lecture");
+
 module.exports = {
-    Auth,
-    BoardGame,
-    BoardGameType,
-    BoardGameMechanic,
-    LoanGame,
-    Publisher,
-    Mechanic,
-    Type,
-    User
+  Auth,
+  BoardGame,
+  BoardGameType,
+  BoardGameMechanic,
+  LoanGame,
+  Publisher,
+  Mechanic,
+  Type,
+  User,
+  Table,
+  Reservation,
+  Event,
+  Feedback,
+  Lecture
 };
