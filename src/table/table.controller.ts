5ea945ddfa18278f10253f992d7f9d34ec88d738
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2019-11-14 21:59:08 +0100

    add controller and service for Table

---
+++ src/table/table.controller.ts
@@ -0,0 +1,69 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiCreatedResponse, ApiImplicitParam, ApiOkResponse, ApiUseTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { TableDto } from './dto/table.dto';
+import { CreateTableDto } from './dto/create-table.dto';
+import { UpdateTableDto } from './dto/update-table.dto';
+import { TableOffset } from './dto/table.offset';
+import { Table as Tabela } from './table.entity';
+import { TableService } from './table.service';
+
+@Controller('tables')
+@ApiUseTags('tables')
+export class TablesController {
+    constructor(private readonly tablesService: TableService) { }
+
+    @Get()
+    @ApiOkResponse({ type: [TableDto] })
+    findAll(): Promise<TableDto[]> {
+        return this.tablesService.findAll();
+    }
+
+    @Get(':id')
+    @ApiOkResponse({ type: TableDto })
+    @ApiImplicitParam({ name: 'id', required: true })
+    findOne(@Param('id', new ParseIntPipe()) id: number): Promise<TableDto> {
+        return this.tablesService.findOne(id);
+    }
+
+    @Post()
+    @ApiCreatedResponse({ type: Tabela })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    create(
+        @Body() createPublisherDto: CreateTableDto,
+    ): Promise<Tabela> {
+        return this.tablesService.create(CreateTableDto);
+    }
+
+    @Put(':id')
+    @ApiOkResponse({ type: Tabela })
+    @ApiImplicitParam({ name: 'id', required: true })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    update(
+        @Param('id', new ParseIntPipe()) id: number,
+        @Body() UpdateTableDto: UpdateTableDto,
+    ): Promise<Tabela> {
+        return this.tablesService.update(id, UpdateTableDto);
+    }
+
+    @Delete(':id')
+    @ApiOkResponse({ type: Tabela })
+    @ApiImplicitParam({ name: 'id', required: true })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    delete(
+        @Param('id', new ParseIntPipe()) id: number,
+    ): Promise<Tabela> {
+        return this.tablesService.delete(id);
+    }
+
+    @Get(':id')
+    @ApiOkResponse({ type: TableOffset })
+    offset(@Param('id', new ParseIntPipe()) index: number = 0): Promise<TableOffset> {
+        return this.tablesService.offset(index);
+    }
+
+
+}
---
+++ src/table/table.service.ts
@@ -0,0 +1,98 @@
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { Table } from 'src/table/table.entity';
+import { TableDto } from 'src/table/dto/table.dto';
+import { CreateTableDto } from 'src/table/dto/create-table.dto';
+import { UpdateTableDto } from 'src/table/dto/update-table.dto';
+import { TableOffset } from 'src/table/dto/table.offset';
+import { User } from 'src/users/user.entity';
+
+@Injectable()
+export class TableService {
+    constructor(
+        @Inject('TablesRepository')
+        private readonly tablesRepository: typeof Table,
+    ) { }
+
+    async findAll(): Promise<TableDto[]> {
+        const tables = await this.tablesRepository.findAll<Table>({
+            include: [User],
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+        });
+        return tables.map(table => {
+            return new TableDto(table);
+        });
+    }
+
+    async findOne(id: number): Promise<TableDto> {
+        const table = await this.tablesRepository.findByPk<Table>(id, {
+            include: [User],
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+        });
+        if (!table) {
+            throw new HttpException('No table found', HttpStatus.NOT_FOUND);
+        }
+
+        return new TableDto(table);
+    }
+
+    async create(CreateTableDto: CreateTableDto): Promise<Table> {
+        const table = new Table();
+        table.name = CreateTableDto.name;
+
+        try {
+            return await table.save();
+        } catch (err) {
+            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private async getTable(id: number): Promise<Table> {
+        const table = await this.tablesRepository.findByPk<Table>(id, {
+            include: [User],
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+        });
+        if (!table) {
+            throw new HttpException('No table found', HttpStatus.NOT_FOUND);
+        }
+
+        return table;
+    }
+
+    async update(
+        id: number,
+        UpdateTableDto: UpdateTableDto,
+    ): Promise<Table> {
+        const table = await this.getTable(id);
+
+        table.name = UpdateTableDto.name || table.name;
+
+        try {
+            return await table.save();
+        } catch (err) {
+            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async delete(id: number): Promise<Table> {
+        const table = await this.getTable(id);
+        await table.destroy();
+        return table;
+    }
+
+    async offset(index: number = 0): Promise<TableOffset> {
+        const tables = await this.tablesRepository.findAndCountAll({
+            include: [User],
+            limit: 100,
+            offset: index * 100,
+            order: ['id'],
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+        });
+
+        const TableDto = tables.rows.map(privilege => {
+            return new TableDto(privilege);
+        });
+
+        return { rows: TableDto, count: TableDto.count };
+    }
+
+}
