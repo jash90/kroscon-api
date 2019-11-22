0d30324d6f3fa0117c71274aa93e75a34cbc8c88
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-11-22 02:20:12 +0100

    type

---
+++ src/type/types.controller.ts
@@ -0,0 +1,69 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiCreatedResponse, ApiImplicitParam, ApiOkResponse, ApiUseTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { TypeDto } from './dto/type.dto';
+import { CreateTypeDto } from './dto/create-type.dto';
+import { UpdateTypeDto } from './dto/update-type.dto';
+import { TypeOffset } from './dto/type.offset';
+import { Type as TypeEntity } from './type.entity';
+import { TypesService } from './types.service';
+
+@Controller('types')
+@ApiUseTags('types')
+export class TypesController {
+    constructor(private readonly typesService: TypesService) { }
+
+    @Get()
+    @ApiOkResponse({ type: [TypeDto] })
+    findAll(): Promise<TypeDto[]> {
+        return this.typesService.findAll();
+    }
+
+    @Get(':id')
+    @ApiOkResponse({ type: TypeDto })
+    @ApiImplicitParam({ name: 'id', required: true })
+    findOne(@Param('id', new ParseIntPipe()) id: number): Promise<TypeDto> {
+        return this.typesService.findOne(id);
+    }
+
+    @Post()
+    @ApiCreatedResponse({ type: TypeEntity })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    create(
+        @Body() createTypeDto: CreateTypeDto,
+    ): Promise<TypeEntity> {
+        return this.typesService.create(createTypeDto);
+    }
+
+    @Put(':id')
+    @ApiOkResponse({ type: TypeEntity })
+    @ApiImplicitParam({ name: 'id', required: true })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    update(
+        @Param('id', new ParseIntPipe()) id: number,
+        @Body() updateTypeDto: UpdateTypeDto,
+    ): Promise<TypeEntity> {
+        return this.typesService.update(id, updateTypeDto);
+    }
+
+    @Delete(':id')
+    @ApiOkResponse({ type: TypeEntity })
+    @ApiImplicitParam({ name: 'id', required: true })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    delete(
+        @Param('id', new ParseIntPipe()) id: number,
+    ): Promise<TypeEntity> {
+        return this.typesService.delete(id);
+    }
+
+    @Get(':id')
+    @ApiOkResponse({ type: TypeOffset })
+    offset(@Param('id', new ParseIntPipe()) index: number = 0): Promise<TypeOffset> {
+        return this.typesService.offset(index);
+    }
+
+
+}
---
+++ src/type/types.service.ts
@@ -0,0 +1,98 @@
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { Type } from 'src/type/type.entity';
+import { TypeDto } from 'src/type/dto/type.dto';
+import { User } from 'src/users/user.entity';
+import { CreateTypeDto } from 'src/type/dto/create-type.dto';
+import { UpdateTypeDto } from 'src/type/dto/update-type.dto';
+import { TypeOffset } from 'src/type/dto/type.offset';
+
+@Injectable()
+export class TypesService {
+    constructor(
+        @Inject('TypesRepository')
+        private readonly typesRepository: typeof Type,
+    ) { }
+
+    async findAll(): Promise<TypeDto[]> {
+        const types = await this.typesRepository.findAll<Type>({
+            include: [User],
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+        });
+        return types.map(type => {
+            return new TypeDto(type);
+        });
+    }
+
+    async findOne(id: number): Promise<TypeDto> {
+        const type = await this.typesRepository.findByPk<Type>(id, {
+            include: [User],
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+        });
+        if (!type) {
+            throw new HttpException('No type found', HttpStatus.NOT_FOUND);
+        }
+
+        return new TypeDto(type);
+    }
+
+    async create(createTypeDto: CreateTypeDto): Promise<Type> {
+        const type = new Type();
+        type.name = createTypeDto.name;
+
+        try {
+            return await type.save();
+        } catch (err) {
+            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private async getType(id: number): Promise<Type> {
+        const type = await this.typesRepository.findByPk<Type>(id, {
+            include: [User],
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+        });
+        if (!type) {
+            throw new HttpException('No type found', HttpStatus.NOT_FOUND);
+        }
+
+        return type;
+    }
+
+    async update(
+        id: number,
+        updateTypeDto: UpdateTypeDto,
+    ): Promise<Type> {
+        const type = await this.getType(id);
+
+        type.name = updateTypeDto.name || type.name;
+
+        try {
+            return await type.save();
+        } catch (err) {
+            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async delete(id: number): Promise<Type> {
+        const type = await this.getType(id);
+        await type.destroy();
+        return type;
+    }
+
+    async offset(index: number = 0): Promise<TypeOffset> {
+        const types = await this.typesRepository.findAndCountAll({
+            include: [User],
+            limit: 100,
+            offset: index * 100,
+            order: ['id'],
+            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
+        });
+
+        const TypesDto = types.rows.map(type => {
+            return new TypesDto(type);
+        });
+
+        return { rows: TypesDto, count: types.count };
+    }
+
+}
